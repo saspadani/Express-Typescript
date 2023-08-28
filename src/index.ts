6fc2cf29ab8502e171688d801ecb660277033cbc
saspadani <aspadani@gmail.com>
2023-08-28 16:07:18 +0700

    index

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { type Application } from 'express';
+import { routes } from './routes';
+
+const app: Application = express();
+const port: number = 4000;
+
+routes(app);
+
+app.listen(port, () =>
+  console.log(`Server is listening on http://localhost:${port}`)
+);
